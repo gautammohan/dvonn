d67692c4f62b2c1be1b7dbbf8ba8b9c5100836a5
emilydiana <emily.r.diana@gmail.com>
2019-11-20 11:48:27 -0500

    Added a placeholder IOInterface

---
+++ src/IOInterface.hs
@@ -0,0 +1,17 @@
+-------------------------------------------------------------------------------
+-- Authors: Emily Diana and Gautam Mohan
+-- Date:
+-- Assignment: Final Project
+------------------------------------------------------------------------------
+
+module IOInterface where
+
+import Defs
+
+--TODO: What is the type signature here?
+--parseMove :: Move -> ??
+parseMove = undefined
+
+--TODO: What is the type signature here?
+--printBoard :: Board -> ??
+printBoard = undefined
