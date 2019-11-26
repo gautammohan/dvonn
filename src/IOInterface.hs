3b28b3e1a580a34402ab4b2bc12e03a8d4c2865e
emilydiana <emily.r.diana@gmail.com>
2019-11-25 23:27:48 -0500

    Some very early board printing pieces

--- src/IOInterface.hs
+++ src/IOInterface.hs
@@ -7,11 +7,55 @@
 module IOInterface where
 
 import Defs
-
+import System.IO
+import Board
+import Tests
 --TODO: What is the type signature here?
 --parseMove :: Move -> ??
 parseMove = undefined
 
 --TODO: What is the type signature here?
 --printBoard :: Board -> ??
-printBoard = undefined
+printHexagon :: Board -> Coordinate -> IO ()
+printHexagon b c = do
+               let s = innerstack b c
+                   color = case s of
+                     (x:xs) -> case head s of
+                        White -> "W"
+                        Black -> "B"
+                        _     -> "R"
+                     _      -> " "
+                   len = case s of 
+                     (x:xs) -> show (length s)
+                     _      -> " "
+               putStrLn(" --- ")
+               putStrLn("/ " ++ color ++ " \\")
+               putStrLn("\\ " ++ len ++ " /")
+               putStrLn(" --- ")
+
+printHexagon2 :: Board -> Coordinate -> IO ()
+printHexagon2 b c = do
+               let s = innerstack b c
+                   color = case s of
+                     (x:xs) -> case head s of
+                        White -> "W"
+                        Black -> "B"
+                        _     -> "R"
+                     _      -> " "
+                   len = case s of 
+                     (x:xs) -> show (length s)
+                     _      -> " "
+               putStrLn(" /'\\ ")
+               putStrLn("|" ++ color ++ len ++ " |")
+               putStrLn(" \\-/ ")
+
+printTop :: IO ()
+printTop = putStrLn(" / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\ ")
+
+printSecondLine :: Board -> IO ()
+printSecondLine b = putStrLn("|   |   |   |   |   |   |   |   |   |")
+
+printGrid :: Board -> IO ()
+printGrid b = do
+            printTop
+            printSecondLine b
