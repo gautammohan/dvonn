ed586d83db57d6276b30e423150327fea1e00b58
emilydiana <emily.r.diana@gmail.com>
2019-12-06 17:31:25 -0500

    Asterisks and axis labels

--- src/IOInterface.hs
+++ src/IOInterface.hs
@@ -11,18 +11,31 @@ import System.IO
 import Board
 import Data.Set as S hiding (foldr)
 
-printTops :: Int -> Int -> IO ()
-printTops n s = do
-           let ns = [1 .. n]
+printTops :: Board -> Int -> Int -> IO ()
+printTops b s i = do
+           let c = S.toList $ S.filter (\(Coordinate (x,y)) -> y == i) (coordinates b)
                ss = case s of
                   0 -> []
                   _ -> [1 ..s]
-               tops = foldr (\x acc-> " / \\" ++ acc) " " ns
+               tops = foldr (\x acc-> " /" ++ r b x ++ "\\" ++ acc) " " c 
+                  where r b x = if containsRed b x then "*" else " "
                spaces = foldr (\x acc-> " " ++ acc) "" ss
            putStrLn(spaces ++ tops)
 
-printBases :: Int -> Int -> IO ()
-printBases n s = do
+printLowerTops :: Board -> Int -> Int -> IO ()
+printLowerTops b s i = do
+           let c = S.toList $ S.filter (\(Coordinate (x,y)) -> y == i) (coordinates b)
+               ss = case s of
+                  0 -> []
+                  _ -> [1 ..s]
+               tops = foldr (\x acc-> " /" ++ r b x ++ "\\" ++ acc) " " c 
+                  where r b x = if containsRed b x then "*" else " "
+               fullTop = " \\" ++ tops ++ "/"
+               spaces = foldr (\x acc-> " " ++ acc) "" ss
+           putStrLn(spaces ++ fullTop)
+
+printBases :: Board -> Int -> Int -> IO ()
+printBases b n s = do
            let ns = [1 .. n]
                ss = case s of
                   0 -> []
@@ -39,7 +52,7 @@ printMiddles b s i = do
                    _ -> [1 .. s]
                 middles = foldr (\x acc -> "|" ++ label x ++ acc) "|" c
                 spaces = foldr (\x acc-> " " ++ acc) "" ss
-            putStrLn(spaces ++ middles) where
+            putStrLn(spaces ++ middles ++ spaces ++ " " ++ show i) where
        label y = case innerstack b y of
           [] -> "   "
           s -> do
@@ -52,43 +65,51 @@ printMiddles b s i = do
 
 -- | Prints normal Dvonn sized board. Can print an emty board, for example
 -- with the command "printGridDvonn emptyDvonn"
+printTopLabelsDvonn :: IO ()
+printTopLabelsDvonn = putStrLn("        A   B   C   D   E   F   G   H   I")
+
+printBottomLabelsDvonn :: IO ()
+printBottomLabelsDvonn = putStrLn("    C   D   E   F   G   H   I   J   K")
 
 printGridDvonn :: Board -> IO ()
 printGridDvonn b = do
-            printTops 9 4
+            printTopLabelsDvonn
+            printTops b 4 1
             printMiddles b 4 1
-            printTops 10 2
+            printTops b 2 2
             printMiddles b 2 2
-            printTops 11 0
+            printTops b 0 3
             printMiddles b 0 3
-            printBases 11 0
+            printLowerTops b 0 4
             printMiddles b 2 4
-            printBases 10 2
+            printLowerTops b 2 5
             printMiddles b 4 5
-            printBases 9 4
+            printBases b 9 4
+            printBottomLabelsDvonn
 
 -- | Prints mini board. Can print an empty mini board, for example
 -- with the command "printGridMini emptyMini"
 --
-printTopsMini :: Int -> Int -> IO ()
-printTopsMini n s = do
-           let ns = [1 .. n]
+printTopsMini :: Board -> Int -> Int -> IO ()
+printTopsMini b s i = do
+           let c = S.toList $ S.filter (\(Coordinate (x,y)) -> y == i) (coordinates b)
                ss = case s of
                   0 -> []
                   _ -> [1 ..s]
-               tops = foldr (\x acc-> " / \\" ++ acc) " " ns
+               tops = foldr (\x acc-> " /" ++ r b x ++ "\\" ++ acc) " " c 
+                  where r b x = if containsRed b x then "*" else " "
                spaces = foldr (\x acc-> " " ++ acc) "" ss
            putStrLn(spaces ++ tops ++ "/")
-
+           
 printGridMini :: Board -> IO ()
 printGridMini b = do
-            printTops 3 4
+            printTops b 4 1
             printMiddles b 4 1
-            printTopsMini 3 2
+            printTopsMini b 2 2
             printMiddles b 2 2
-            printTopsMini 3 0
+            printTopsMini b 0 3
             printMiddles b 0 3
-            printBases 3 0
+            printBases b 3 0
 
 printBoard :: Board -> IO ()
 printBoard b = if size (coordinates b) == 9 then printGridMini b else printGridDvonn b
