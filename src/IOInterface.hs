7328d241991c669c13f5da651c4f8b56eb4a4bb4
emilydiana <emily.r.diana@gmail.com>
2019-12-06 21:17:11 -0500

    More printing functionality

--- src/IOInterface.hs
+++ src/IOInterface.hs
@@ -14,45 +14,44 @@ import Data.Set as S hiding (foldr)
 printTops :: Board -> Int -> Int -> IO ()
 printTops b s i = do
            let c = S.toList $ S.filter (\(Coordinate (x,y)) -> y == i) (coordinates b)
-               ss = case s of
-                  0 -> []
-                  _ -> [1 ..s]
                tops = foldr (\x acc-> " /" ++ r b x ++ "\\" ++ acc) " " c 
                   where r b x = if containsRed b x then "*" else " "
-               spaces = foldr (\x acc-> " " ++ acc) "" ss
-           putStrLn(spaces ++ tops)
+           putStrLn(replicate s ' ' ++ tops)
 
 printLowerTops :: Board -> Int -> Int -> IO ()
 printLowerTops b s i = do
            let c = S.toList $ S.filter (\(Coordinate (x,y)) -> y == i) (coordinates b)
-               ss = case s of
-                  0 -> []
-                  _ -> [1 ..s]
                tops = foldr (\x acc-> " /" ++ r b x ++ "\\" ++ acc) " " c 
                   where r b x = if containsRed b x then "*" else " "
                fullTop = " \\" ++ tops ++ "/"
-               spaces = foldr (\x acc-> " " ++ acc) "" ss
-           putStrLn(spaces ++ fullTop)
+           putStrLn(replicate s ' ' ++ fullTop)
 
 printBases :: Board -> Int -> Int -> IO ()
 printBases b n s = do
            let ns = [1 .. n]
-               ss = case s of
-                  0 -> []
-                  _ -> [1 ..s]
                bases = foldr (\x acc-> " \\ /" ++ acc) " " ns
-               spaces = foldr (\x acc-> " " ++ acc) "" ss
-           putStrLn(spaces ++ bases)
+           putStrLn(replicate s ' ' ++ bases)
 
-printMiddles :: Board -> Int -> Int -> IO ()
-printMiddles b s i = do
+printMiddlesDvonn :: Board -> Int -> Int -> IO ()
+printMiddlesDvonn b s i = do
             let c = S.toList $ S.filter (\(Coordinate (x,y)) -> y == i) (coordinates b)
-                ss = case s of
-                   0 -> []
-                   _ -> [1 .. s]
                 middles = foldr (\x acc -> "|" ++ label x ++ acc) "|" c
-                spaces = foldr (\x acc-> " " ++ acc) "" ss
-            putStrLn(spaces ++ middles ++ spaces ++ " " ++ show i) where
+            putStrLn(replicate s ' ' ++ middles ++ replicate s ' ' ++ " " ++ show i) where
+       label y = case innerstack b y of
+          [] -> "   "
+          s -> do
+                let col = case head s of
+                      White -> "W"
+                      Black -> "B"
+                      Red   -> "R"
+                    size = length s
+                if size < 10 then col ++ " " ++ show size else col ++ show size
+
+printMiddlesMini :: Board -> Int -> Int -> IO ()
+printMiddlesMini b s i = do
+            let c = S.toList $ S.filter (\(Coordinate (x,y)) -> y == i) (coordinates b)
+                middles = foldr (\x acc -> "|" ++ label x ++ acc) "|" c
+            putStrLn(replicate s ' ' ++ middles ++ replicate (5 - s) ' ' ++ show i) where
        label y = case innerstack b y of
           [] -> "   "
           s -> do
@@ -71,19 +70,25 @@ printTopLabelsDvonn = putStrLn("        A   B   C   D   E   F   G   H   I")
 printBottomLabelsDvonn :: IO ()
 printBottomLabelsDvonn = putStrLn("    C   D   E   F   G   H   I   J   K")
 
+printTopLabelsMini :: IO ()
+printTopLabelsMini = putStrLn("        A   B   C ")
+
+printBottomLabelsMini :: IO ()
+printBottomLabelsMini = putStrLn("A   B   C ")
+
 printGridDvonn :: Board -> IO ()
 printGridDvonn b = do
             printTopLabelsDvonn
             printTops b 4 1
-            printMiddles b 4 1
+            printMiddlesDvonn b 4 1
             printTops b 2 2
-            printMiddles b 2 2
+            printMiddlesDvonn b 2 2
             printTops b 0 3
-            printMiddles b 0 3
+            printMiddlesDvonn b 0 3
             printLowerTops b 0 4
-            printMiddles b 2 4
+            printMiddlesDvonn b 2 4
             printLowerTops b 2 5
-            printMiddles b 4 5
+            printMiddlesDvonn b 4 5
             printBases b 9 4
             printBottomLabelsDvonn
 
@@ -93,23 +98,21 @@ printGridDvonn b = do
 printTopsMini :: Board -> Int -> Int -> IO ()
 printTopsMini b s i = do
            let c = S.toList $ S.filter (\(Coordinate (x,y)) -> y == i) (coordinates b)
-               ss = case s of
-                  0 -> []
-                  _ -> [1 ..s]
                tops = foldr (\x acc-> " /" ++ r b x ++ "\\" ++ acc) " " c 
                   where r b x = if containsRed b x then "*" else " "
-               spaces = foldr (\x acc-> " " ++ acc) "" ss
-           putStrLn(spaces ++ tops ++ "/")
+           putStrLn(replicate s ' ' ++ tops ++ "/")
            
 printGridMini :: Board -> IO ()
 printGridMini b = do
+            printTopLabelsMini
             printTops b 4 1
-            printMiddles b 4 1
+            printMiddlesMini b 4 1
             printTopsMini b 2 2
-            printMiddles b 2 2
+            printMiddlesMini b 2 2
             printTopsMini b 0 3
-            printMiddles b 0 3
+            printMiddlesMini b 0 3
             printBases b 3 0
+            printBottomLabelsMini
 
 printBoard :: Board -> IO ()
 printBoard b = if size (coordinates b) == 9 then printGridMini b else printGridDvonn b
