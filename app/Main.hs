dcb0b76e36fa0f84f2571e0a0f8141f6c34044c0
Gautam Mohan <s.gautam.mohan@gmail.com>
2019-12-17 11:08:27 -0500

    fix buffering for running main in terminal

--- app/Main.hs
+++ app/Main.hs
@@ -3,9 +3,12 @@ module Main where
 import Game
 import Control.Monad.State (evalState)
 import Defs
+import System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdin NoBuffering
+  hSetBuffering stdout NoBuffering
   w <-  evalGame startState dvonn
   case w of
     Right (winner, _) -> putStrLn $ "The winner is: " ++ show winner
